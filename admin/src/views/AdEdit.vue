7eeaea0cd96d3855e9a1895d349b17dfd0d34d8c
itAzeY <953463876@qq.com>
2019-11-17 19:30:20 +0800

    首页关灯片

--- admin/src/views/AdEdit.vue
+++ admin/src/views/AdEdit.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="categories">
-    <h1>{{id ? '编辑分类' : '新建分类'}}</h1>
+    <h1>{{id ? '编辑' : '新建'}}广告位</h1>
     <el-form label-width="120px" @submit.native.prevent="handleSave">
-      <el-form-item label="上级分类">
-        <!-- <el-input v-model="model.parent" placeholder="请输入名称"></el-input> -->
-        <el-select v-model="model.parent">
-          <el-option v-for="item in parents" :key="item._id" :value="item._id" :label="item.name"></el-option>
-        </el-select>
+      <el-form-item label="广告位名称">
+        <el-input v-model="model.name" placeholder="请输入广告位名称"></el-input>
       </el-form-item>
-      <el-form-item label="分类名称">
-        <el-input v-model="model.name" placeholder="请输入名称"></el-input>
+      <el-form-item label="广告">
+        <el-button type="text" @click="model.items.push({})">
+          <i class="el-icon-plus"></i>添加广告
+        </el-button>
+        <el-row type="flex" style="flex-wrap: wrap;">
+          <el-col :md="24 " v-for="(item, index ) of model.items" :key="index" class="el-col-border">
+            <el-form-item label="URL">
+              <el-input v-model="item.url"></el-input>
+            </el-form-item>
+            <el-form-item label="图片" style="margin-top:1rem"> 
+              <el-upload
+                class="avatar-uploader"
+                :action="$http.defaults.baseURL + '/upload'"
+                :show-file-list="false"
+                :on-success="res => $set(item, 'image', res.url)"
+              >
+                <img v-if="item.image" :src="item.image" class="avatar banner" />
+                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+              </el-upload>
+            </el-form-item>
+            <el-form-item>
+              <el-button size="small" type="danger">删除</el-button>
+            </el-form-item>
+          </el-col>
+        </el-row>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="subbmit">保存</el-button>
@@ -26,34 +46,37 @@ export default {
   },
   data() {
     return {
-      model: {},
-      parents:[]
+      model: {
+        items: []
+      }
     };
   },
   created() {
     this.id && this.fetch();
-    this.fetchParents()
   },
   methods: {
     async handleSave() {
       // eslint-disable-next-line no-unused-vars
       let res;
       if (this.id) {
-        res = await this.$http.put(`rest/categories/${this.id}`, this.model);
+        res = await this.$http.put(`rest/ads/${this.id}`, this.model);
       } else {
-        res = await this.$http.post("rest/categories", this.model);
+        res = await this.$http.post("rest/ads", this.model);
       }
-      this.$router.push("/categories/list");
+      this.$router.push("/ads/list");
       this.$message.success("保存成功");
     },
     async fetch() {
-      const res = await this.$http.get(`rest/categories/${this.id}`);
-      this.model = res.data;
-    },
-    async fetchParents() {
-      const res = await this.$http.get(`rest/categories`);
-      this.parents = res.data;
+      const res = await this.$http.get(`rest/ads/${this.id}`);
+      this.model = Object.assign({}, this.model, res.data);
     }
   }
 };
 </script>
+
+<style>
+.banner{
+  width: auto;
+  height: 10rem;
+}
+</style>
--- admin/src/views/AdList.vue
+++ admin/src/views/AdList.vue
@@ -1,13 +1,12 @@
 <template>
 	<div class="categories">
-		<h1>分类列表</h1>
+		<h1>广告位列表</h1>
 		<el-table :data="items" border>
 			<el-table-column label="ID" prop="_id"></el-table-column>
-			<el-table-column label="上级分类" prop="parent.name"></el-table-column>
-			<el-table-column label="分类名称" prop="name"></el-table-column>
+			<el-table-column label="广告位 名称" prop="name"></el-table-column>
 			<el-table-column>
 				<template slot-scope="{row}">
-					<el-button type="text" @click="$router.push(`/categories/edit/${row._id}`)">编辑</el-button>
+					<el-button type="text" @click="$router.push(`/ads/edit/${row._id}`)">编辑</el-button>
 					<el-button type="text" @click="remove(row)">删除</el-button>
 				</template>
 			</el-table-column>
@@ -28,16 +27,16 @@ export default {
 	},
 	methods: {
 		async fetch(){
-			const items = await this.$http.get('rest/categories')
+			const items = await this.$http.get('rest/ads')
 			this.items = items.data.data
 		},
 		async remove(row){
-			this.$confirm(`是否要删除此分类----${row.name}`, '提示', {
+			this.$confirm(`是否要删除此广告位----${row.name}`, '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
         }).then(async () => {
-					const res = await this.$http.delete(`rest/categories/${row._id}`)
+					const res = await this.$http.delete(`rest/ads/${row._id}`)
           if(res){
 						this.$message.success('删除成功')
 						this.fetch()
